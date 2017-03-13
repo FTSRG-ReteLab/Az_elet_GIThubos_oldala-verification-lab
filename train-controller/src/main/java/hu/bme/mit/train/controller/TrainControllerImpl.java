af42a3063c61ec1bb9d92bf9492ca2e5cc3771ec
Balazsi Gaspar <balazsi.gaspar@gmail.com>
2017-03-13 07:30:01 -0700

    Set reference speed implemented in train controller

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -1,5 +1,8 @@
 package hu.bme.mit.train.controller;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import hu.bme.mit.train.interfaces.TrainController;
 
 public class TrainControllerImpl implements TrainController {
@@ -7,6 +10,19 @@ public class TrainControllerImpl implements TrainController {
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
+	
+	private Timer timer;
+	
+	public TrainControllerImpl(){
+		timer=new Timer();
+		timer.schedule(new FollowSpeedTask(), 1000);
+	}
+	
+	class FollowSpeedTask extends TimerTask {
+		public void run(){
+			followSpeed();
+		}
+	}
 
 	@Override
 	public void followSpeed() {
@@ -43,7 +59,8 @@ public class TrainControllerImpl implements TrainController {
 
 	@Override
 	public void setJoystickPosition(int joystickPosition) {
-		this.step = joystickPosition;		
+
+		this.step = joystickPosition;
 	}
 
 }
